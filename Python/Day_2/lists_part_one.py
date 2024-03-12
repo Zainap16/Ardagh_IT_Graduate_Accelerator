28f17f56202122a844785b38e1eeea7ddc109144
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-03-12 12:49:02 +0200

    canvas_intro

---
+++ Python/Day_2/lists_part_one.py
@@ -0,0 +1,12 @@
+#  this is a list
+letters = ['a','b','c']
+# list within a list
+matrix = [[0,1],[[2,3]]]
+
+#  zeros = [0] * 10 -->> prints 10 0's
+
+# 20 NOT included
+numbers = list(range(20))
+greet = list('Hello World')
+print(numbers)
+print(greet)
