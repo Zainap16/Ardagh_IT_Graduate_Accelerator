44c2e5163d876b97c50e5faf2859aaa84b1d6dc7
Zainap16 <zvanblerck1@gmail.com>
2024-03-13 23:21:04 +0200

    lists

---
+++ 2D_lists.py
@@ -0,0 +1,12 @@
+food = ['pizza','hamburger','hotdog','spaghetti','pudding']
+# 2D lists -= list of a list
+# for x in food:
+#     print(x, end=" ")
+
+drinks = ["coffee","pizza","tea"] # 0
+dinner = ["pizza","hamburger","hotdog"] # 1
+dessert = ["cake","ice cream"] # 2
+
+food = [drinks, dinner, dessert]
+# food[which list][what element in a list]
+
