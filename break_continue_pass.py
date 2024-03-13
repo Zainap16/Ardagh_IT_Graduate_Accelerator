947907c9c253adfd6d5b47e1ce6916b9c4130984
Zainap16 <zvanblerck1@gmail.com>
2024-03-13 23:10:01 +0200

    break_notes_stuff

---
+++ break_continue_pass.py
@@ -0,0 +1,3 @@
+# break = used to terminate loop
+#  continue = skips the next iteration of the loop 
+#  pass = does nothing, acts as a placeholder (skips something)
---
+++ nested_loops.py
@@ -0,0 +1,13 @@
+rows = int(input("rows: "))
+columns = int(input("columns: "))
+symbols = input("symbol to use: ")
+
+for r in range(rows):
+    for c in range(columns):
+        print(symbols, end="")
+    print()
+
+
+
+
+
