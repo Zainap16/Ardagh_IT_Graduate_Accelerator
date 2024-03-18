a13ff241d6cf327d2c453d72811103015e6b0771
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-03-18 13:17:51 +0200

    changes

---
+++ greet_presentation.py
@@ -0,0 +1,8 @@
+def greet(name): #parameters
+    print('Hello '+name+' how are you today?')
+
+greet('Mathew')# arguments
+
+def cook_pasta(servings):
+    print('Boil water for '+ servings +' servings of pasta.')
+    # other cooking steps ...
---
+++ parameters.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+def performOperation(num1,num2,operation='sum', message ='Default message'):
+    print(message)
+    if operation=='sum':
+        return num1+num2
+    if operation=='multiply':
+        return num1+num2
+    
+performOperation(2,3,message='A new message', operation='multiply')
+
+
+
+
