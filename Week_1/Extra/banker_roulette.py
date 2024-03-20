626e6eadd936b0fc56367e0302079cd410815374
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-03-20 08:38:35 +0200

    edited_structure

---
+++ Week_1/Extra/banker_roulette.py
@@ -0,0 +1,23 @@
+import random
+# select a random name from a list of names
+
+names = ["Zainap","Kopano","Collen","Shaqeel","Sino"]
+
+names_len = len(names)
+rando_names = random.randint(0,names_len +1)
+choice = names[rando_names]
+print(f"original list: {names}")
+print(f"length of list: {names_len}")
+print(f"random element number in the list: {rando_names}")
+print(f"{choice} is going to buy the meal today!")
+
+
+
+
+
+
+
+
+
+
+
