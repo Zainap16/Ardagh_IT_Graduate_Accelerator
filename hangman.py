9d6824c66dc78111301957b7c4cd54c73cbe9cfe
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-03-26 11:46:05 +0200

    incompleted

--- hangman.py
+++ hangman.py
@@ -1,16 +1,32 @@
 import random
+
 word_list = ["elephant","mouse","camel"]
+iterate = 0 
+tries = 0   
+display =[]
 
 chosen_word = random.choice(word_list)
-word = ''.join(chosen_word)
-print(word)
+print(chosen_word)
 
-guess = input("Guess a letter: ").lower()
+while len(chosen_word) < tries:
+    tries += 1
+    guess = input("Guess a letter: ").lower()
 
-
-for char in word:
-    # print(char)
-    if guess in char:
-        print("yes")
+for letter in chosen_word:
+    iterate+=1
+    # display.insert(count,letter) 
+    display.insert(iterate,"_")
+    if guess in letter:
+        display.insert(iterate, guess)
     else:
-        print("no")    
+        pass
+print(display) 
+
+
+
+ 
+   
+
+
+        
+     
