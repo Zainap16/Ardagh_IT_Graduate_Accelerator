723f74771f9aaff9376b23be40ea7b1cfebe8370
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-03-28 13:25:18 +0200

    D1

--- Week_3/Day_1/day_1.py
+++ Week_3/Day_1/day_1.py
@@ -1,9 +1,15 @@
 '''
 1. find inspiration
-2. user stories
-3. use cases
+
+2. user stories - which depict small scenarios from a user's perspective, these stories should emphasize the user's goal and motivation rather than the application itself. 
+->WHO, WHAT, WHY
+
+3. use cases - typically include a title, an actor (a user or system), and a scenario that describes how a goal is achieved. The scenario can be written as a paragraph or a list of steps in simple language
+-> WHO, WHAT, HOW?
+
 4. project requirements
-5. architecture
+
+5. architecture - For instance, in the functional requirements, words like quote, forecast, location, trends, article, content, email, and recipients stand out as potential objects.
 
 
 
