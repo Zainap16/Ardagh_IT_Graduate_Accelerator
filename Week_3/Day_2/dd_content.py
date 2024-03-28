31d3d3c3db6700983ba82a2ad92126344c08d150
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-03-28 13:56:00 +0200

    incomplete

--- Week_3/Day_2/dd_content.py
+++ Week_3/Day_2/dd_content.py
@@ -1,6 +1,8 @@
 
 def get_random_quote(self):
-    pass
+    try:
+        with open(quotes_file) as csvfile:
+            quotes = 
     
 def get_weather_trends(self):
     pass
@@ -8,5 +10,8 @@ def get_weather_trends(self):
 def get_wikipedia_article(self):
     pass
     
+def get_twitter_article(self):
+    pass    
+
 if __name__ == '__main__':
     pass
---
+++ Week_3/Day_2/dd_gui.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkinter import ttk
+
+class DailyDigustGUI:
+    pass
+
+"""
+GUI should enable the admin to...
+    - config which content source to include in email
+    - add recipients
+    - remove recipients
+    - schedule daily time to send email
+    - config sender credentials
+
+"""
+
+if __name__ == "__main__":
+    root = Tk()
+    app = DailyDigustGUI(root)
+    root.mainloop()
+
+
