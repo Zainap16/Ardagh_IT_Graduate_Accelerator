2228b6c83e01ce20b37013cd9662b0f111114f49
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-03-28 13:42:21 +0200

    content

---
+++ Week_3/Day_2/dd_content.py
@@ -0,0 +1,12 @@
+
+def get_random_quote(self):
+    pass
+    
+def get_weather_trends(self):
+    pass
+    
+def get_wikipedia_article(self):
+    pass
+    
+if __name__ == '__main__':
+    pass
---
+++ Week_3/Day_2/dd_email.py
@@ -0,0 +1,13 @@
+class DailyDigestEmail:
+    
+    def __init__(self):
+        pass
+    
+    def send_email(self):
+        pass
+    
+    def format_message(self):
+        pass
+    
+if __name__ == '__main__':
+    pass
