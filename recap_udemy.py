672c183935a996577283b350435efb0500c0491d
Zainap16 <zvanblerck1@gmail.com>
2024-04-01 23:27:15 +0200

    dict_1

---
+++ recap_udemy.py
@@ -0,0 +1,11 @@
+programming_dictionary = {
+    "Cat" : "feline",
+    "Dog" : "canine"
+}
+
+# view from dict
+print(programming_dictionary["Cat"])
+
+# add dict
+programming_dictionary["Lion"] = "big feline"
+print(programming_dictionary["Lion"])
