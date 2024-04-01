c3942e0298445035df737ae4d94350bb1534b813
Zainap16 <zvanblerck1@gmail.com>
2024-04-01 23:29:36 +0200

    completed_dict_recap

--- recap_udemy.py
+++ recap_udemy.py
@@ -9,3 +9,7 @@ print(programming_dictionary["Cat"])
 # add dict
 programming_dictionary["Lion"] = "big feline"
 print(programming_dictionary["Lion"])
+print("\n\n")
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
