6377baeb0c047309c4fb5dc5d330ec422469b2c0
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-04-03 11:49:02 +0200

    gui_iduts

--- password_manager/login.py
+++ password_manager/login.py
@@ -1,9 +1,20 @@
 import tkinter as tk
-
+from tkinter import messagebox
 window = tk.Tk()
+password_placeholder = "123"
+username_placeholder = "Z"
 
-password_placeholder = "hdgeGH123"
-username_placeholder = "Ziah12"
+def show_error():
+    messagebox.showerror("Error","Incorrect/Invalid username and or password")
+
+def submit_info():
+        if username_placeholder == txt_login_username.get() and     txt_login_password.get() == password_placeholder:
+            
+            messagebox.showinfo("Information", "Login Successful")
+        else:
+            show_error()
+
+
 
 lbl_home = tk.Label(text="Login form")
 lbl_home.pack()
@@ -14,10 +25,17 @@ txt_login_username = tk.Entry()
 lbl_login_password = tk.Label(text="Enter your password")
 txt_login_password = tk.Entry() 
 
+btn_submit = tk.Button(text="Submit", command=submit_info)
+
 lbl_login_username.pack()
 txt_login_username.pack()
 
 lbl_login_password.pack()
 txt_login_password.pack()
+
+btn_submit.pack()
+    
+    
+    
 
 window.mainloop()
